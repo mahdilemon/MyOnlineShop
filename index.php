eea4931ee9d04730e0588c97e1ff887e5170f6eb
MAHDI <mahdi.shariatzadeh.1991@gmail.com>
2025-05-24 11:19:18 +0330

    صفحه ۱۴۱

---
+++ index.php
@@ -0,0 +1,17 @@
+<head>
+<meta charset="UTF-8"/>
+<title>فروشگاه من</title>
+</head>
+
+<body>
+<style type="text/css">
+<!--
+	.set_style_link{
+	text-decoration: none;
+	font-weight: bold;
+	}
+<table>
+</table>
+-->
+</style>
+</body>
